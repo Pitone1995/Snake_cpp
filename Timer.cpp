b3dd82a5c3571b36068b5e7f0d0422eb98a2c733
Pitone1995 <pit19antenucci@gmail.com>
2024-07-07 15:58:03 +0200

    Compute also minutes with duration_cast

--- Timer.cpp
+++ Timer.cpp
@@ -1,14 +1,12 @@
 #include "Timer.h"
 
 Timer::Timer() {
-    
+
 }
 
 void Timer::resetTimer() {
 
     m_begin = std::chrono::steady_clock::now();
-    m_min = {};
-    m_sec = {};
     t_pause = {};
 }
 
@@ -17,22 +15,17 @@ std::string Timer::getElapsedTime() {
     std::chrono::steady_clock::time_point t_now = std::chrono::steady_clock::now();
 
     // Get seconds elapsed from the beginning
-    int m_sec = std::chrono::duration_cast<std::chrono::seconds>(t_now - m_begin).count();
+    int min = std::chrono::duration_cast<std::chrono::minutes>(t_now - m_begin).count();
+    int sec = std::chrono::duration_cast<std::chrono::seconds>(t_now - m_begin).count();
 
-    /* I choose to reset at every minute, so i can reset seconds variable and increment minute variable;
-    maybe there is a better solution */
-    if (m_sec == 60) {
-     
-        m_begin = t_now;
-        m_min++;
-        m_sec = 0;
-    }
-
+    // Stay in the range 0 - 60 s
+    sec -= (60 * min);
+    
     // Graphical representation mm::ss
-    std::string m_minS = (m_min < 10 ? "0" + std::to_string(m_min) : std::to_string(m_min));
-    std::string m_secS = (m_sec < 10 ? "0" + std::to_string(m_sec) : std::to_string(m_sec));
+    std::string minS = (min < 10 ? "0" + std::to_string(min) : std::to_string(min));
+    std::string secS = (sec < 10 ? "0" + std::to_string(sec) : std::to_string(sec));
 
-    return m_minS + ':' + m_secS;    
+    return minS + ':' + secS;    
 }
 
 void Timer::pauseTimer() {
--- Timer.h
+++ Timer.h
@@ -9,7 +9,7 @@ class Timer {
 public:
 
     Timer();
-    
+
     void resetTimer();
     std::string getElapsedTime();
     void restartWDelay();
@@ -17,8 +17,6 @@ public:
 
 private:
 
-    int m_min{};
-    int m_sec{};
     std::chrono::steady_clock::time_point m_begin{};
     std::chrono::steady_clock::time_point t_pause{};
 };
